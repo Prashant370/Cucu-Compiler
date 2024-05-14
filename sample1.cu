6d842fcc332bbf42d45da887ad0e256710c2845c
Prashant Kumar <prashantdos2020@gmail.com>
2024-05-14 22:36:06 +0530

    sample files uploading

---
+++ sample1.cu
@@ -0,0 +1,16 @@
+
+
+int add(){
+    int have = 0;
+    int x = 0;
+    int y = 0;
+    have = x + y;
+    return have;
+}
+int main(){
+    int i = 0;
+    int j;
+    int k = 5;
+    int p;
+    return p;
+}
